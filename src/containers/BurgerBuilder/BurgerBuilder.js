5964f6f3b61c84a10807c8d4f2861164f2ac7c77
Vikrant Sharma <vikkrant.xx7@gmail.com>
2018-12-20 22:59:39 +0530

    Implement Burger component and style

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends React.Component {
     render() {
         return (
             <>
-                <div>Burger</div>
+                <Burger />
                 <div>Burger Controls</div>
             </>
         )
