5b08de072c00a24fe36e379f9a3ddbc77007d101
Vikrant Sharma <vikkrant.xx7@gmail.com>
2019-01-07 01:08:59 +0530

    Fix null ings redirect

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
-import {Route} from 'react-router-dom'
+import {Route, Redirect} from 'react-router-dom'
 import ContactData from '../Checkout/ContactData/ContactData'
 import { connect } from 'react-redux'
 
@@ -15,12 +15,16 @@ class Checkout extends Component {
     }
 
     render() {
+        let summary = <Redirect to='/'/>
+        if(this.props.ings) {
+            summary = <div>
+                        <CheckoutSummary ingredients={this.props.ings} checkoutCancelled={this.checkoutCancelledHandler}
+                            checkoutContinued={this.checkoutContinuedHandler} />
+                        <Route path="/checkout/contact-data" component={ContactData} />
+                    </div>
+        }
         return (
-            <div>
-                <CheckoutSummary ingredients={this.props.ings} checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route path="/checkout/contact-data" component={ContactData} />
-            </div>
+            summary
         )
     }
 }
